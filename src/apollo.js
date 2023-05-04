f3cb862bf62143393c76e79bc62a7f18a95ad40f
Eddie0322 <18085543d@connect.polyu.hk>
2023-05-05 01:07:43 +0800

    Update

--- src/apollo.js
+++ src/apollo.js
@@ -4,7 +4,7 @@ import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
 const scheme = (proto) =>
-  window.location.protocol === "https:" ? `${proto}` : proto;
+  window.location.protocol === "http:" ? `${proto}s` : proto;
 
 const splitter = ({ query }) => {
   const { kind, operation } = getMainDefinition(query) || {};
@@ -19,7 +19,7 @@ const options = { reconnect: true };
 const HASURA_SECRET = "eKUUl0j58WBR1753anBeFEUL53fiteGS1GLH1DwP5pboiNNCVilrIAW7Tqnni14x";
 
 const wsURI = `${scheme("ws")}://${GRAPHQL_ENDPOINT}/v1/graphql`;
-const httpurl = `${scheme("https")}://${GRAPHQL_ENDPOINT}/v1/graphql`;
+const httpurl = `${scheme("http")}://${GRAPHQL_ENDPOINT}/v1/graphql`;
 
 const wsLink = new GraphQLWsLink(createClient({ url: wsURI, connectionParams: { options, headers:{ 'x-hasura-admin-secret': HASURA_SECRET } } }));
 const httpLink = new HttpLink({ uri: httpurl, headers:{'x-hasura-admin-secret': HASURA_SECRET} });
